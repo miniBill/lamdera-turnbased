7654a489ab24599493aec6de9953352768bbdb9f
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-08-21 23:47:41 +0200

    Cleaner

--- src/Pages/Fate.elm
+++ src/Pages/Fate.elm
@@ -85,62 +85,66 @@ subscriptions _ =
 view : Shared.Model -> Model -> View Msg
 view _ model =
     { kind = Fate
-    , body =
-        Theme.column
-            [ centerX
-            , height fill
-            ]
-            [ Theme.fateTitle
-            , Theme.column [ centerX, centerY ]
-                [ Input.text
-                    [ Font.center
-                    , centerY
-                    , Background.color Theme.colors.fateBackground
-                    , Border.color Theme.colors.fate
-                    ]
-                    { label =
-                        Input.labelAbove [ centerX ] <|
-                            paragraph
-                                [ Theme.fonts.gotham ]
-                                [ text "GAME NAME" ]
-                    , onChange = Input
-                    , text = model.input
-                    , placeholder =
-                        Maybe.map
-                            (\placeholder ->
-                                Input.placeholder
-                                    [ Font.color <| rgb255 0xA8 0xAA 0xA5 ]
-                                    (text placeholder)
-                            )
-                            model.placeholder
-                    }
-                , if String.length model.input > 5 then
-                    link
-                        [ centerX
-                        , Border.width 1
-                        , Theme.padding
-                        , Border.rounded Theme.rythm
-                        , Theme.fonts.gotham
-                        ]
-                        { url =
-                            Route.toString
-                                { path = Route.Path.Fate_Id_ { id = model.input }
-                                , query = Dict.empty
-                                , hash = Nothing
-                                }
-                        , label = text "JOIN GAME"
-                        }
-
-                  else
-                    el
-                        [ Theme.padding
-                        , Border.width 1
-                        , Border.color Theme.colors.fateBackground
-                        ]
-                        (text " ")
-                ]
-            ]
+    , body = body model
     }
+
+
+body : Model -> Theme.Element Msg
+body model =
+    Theme.column
+        [ centerX
+        , height fill
+        ]
+        [ Theme.fateTitle
+        , Theme.column [ centerX, centerY ]
+            [ Input.text
+                [ Font.center
+                , centerY
+                , Background.color Theme.colors.fateBackground
+                , Border.color Theme.colors.fate
+                ]
+                { label =
+                    Input.labelAbove [ centerX ] <|
+                        paragraph
+                            [ Theme.fonts.gotham ]
+                            [ text "GAME NAME" ]
+                , onChange = Input
+                , text = model.input
+                , placeholder =
+                    Maybe.map
+                        (\placeholder ->
+                            Input.placeholder
+                                [ Font.color <| rgb255 0xA8 0xAA 0xA5 ]
+                                (text placeholder)
+                        )
+                        model.placeholder
+                }
+            , if String.length model.input > 5 then
+                link
+                    [ centerX
+                    , Border.width 1
+                    , Theme.padding
+                    , Border.rounded Theme.rythm
+                    , Theme.fonts.gotham
+                    ]
+                    { url =
+                        Route.toString
+                            { path = Route.Path.Fate_Id_ { id = model.input }
+                            , query = Dict.empty
+                            , hash = Nothing
+                            }
+                    , label = text "JOIN GAME"
+                    }
+
+              else
+                el
+                    [ Theme.padding
+                    , Border.width 1
+                    , Border.color Theme.colors.fateBackground
+                    ]
+                    (text " ")
+            ]
+        ]
 
 
 updateFromBackend : ToFrontendPage -> Model -> ( Model, Cmd Msg )
